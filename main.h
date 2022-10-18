2055e677ea34d6cd9094899c272930ebd1ebf686
Eyethu <111011922+Eyethu30@users.noreply.github.com>
2022-10-18 21:20:05 +0200

    Update main.h

--- main.h
+++ main.h
@@ -8,7 +8,7 @@ int _printf(const char *format, ...);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_int(va_list arg);
-int print_address(va_list arg);
+int print_ptr(va_list arg);
 int print_unsigned(va_list arg);
 int print_unsignedToBinary(va_list arg);
 int print_oct(va_list arg);
