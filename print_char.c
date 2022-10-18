70011bd2d04b90daf64b29476967d141b2a157cd
Riana <rianamoi@gmail.com>
2022-10-18 05:36:20 -0700

    update to printf project

---
+++ print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_char - writes the character c to stdout
+ * @arg: argument
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_char(va_list arg)
+
+{
+	return (_putchar(va_arg(arg, int)));
+}
