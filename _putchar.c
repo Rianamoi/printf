8843debf5ab55861ca6cf4baa7bd1003fe3bcef2
Eyethu <followme303030@gmail.com>
2022-10-18 11:51:37 +0200

    new

--- _putchar.c
+++ _putchar.c
@@ -1,21 +1,17 @@
-#include <unistd.h>
-
+#include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: 0 on success and -1 error and errno is set appropiately
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
-<<<<<<< HEAD
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
-=======
-
-int _putchar(char c)
-
+char *sh = "_putchar";
+while (*sh)
 {
-
-	return (write(1, &c, 1));
-
->>>>>>> 99376cf98fe1bf2b6c925a48a01203c01b49c840
+	putchar(*sh);
+	sh++;
+}
+putchar('\n');
+return (0);
 }
